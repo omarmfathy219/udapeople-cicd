31bb1644602cc5476bcfa548b6e92d09a83f6409
Omar M. Fathy <OmarMFathy219@gmail.com>
2023-01-02 01:02:43 +0200

    f

---
+++ backend/set_env.sh
@@ -0,0 +1,13 @@
+export NODE_ENV=production
+export VERSION=1
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_MIGRATIONS_DIR=./dist/migrations
+export TYPEORM_ENTITIES=./modules/domain/**/*.entity(.ts,.js)
+export TYPEORM_MIGRATIONS=./dist/migrations/*(.ts,.js)
+
+
+export TYPEORM_HOST=localhost
+export TYPEORM_PORT=5532
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=password
+export TYPEORM_DATABASE=glee
